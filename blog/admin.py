541065d04f786dac9a0135d26ea4f05fb04578af
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-07-18 14:49:05 +0300

    model MyBlog

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from blog.models import MyBlog
+
+
+@admin.register(MyBlog)
+class MeBlogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'content', 'image', 'date_create')
